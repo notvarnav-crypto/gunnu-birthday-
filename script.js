766bdaf06476ae8aae67d65ad01d604867ed1b1a
notvarnav-crypto <notvarnav@gmail.com>
2025-12-19 14:14:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function reveal() {
   document.querySelector('.hero').style.display = 'none';
   document.getElementById('story').classList.remove('hidden');
 }
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.innerHTML = "❤";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = 4 + Math.random() * 4 + "s";
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 6000);
+}
+
+setInterval(createHeart, 500);
